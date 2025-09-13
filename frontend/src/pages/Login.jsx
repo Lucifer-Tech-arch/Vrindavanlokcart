e4957e17e85f2f6d5f5abd95f578dc82c6fed38e
Krishna Goswami <www.goswamikrishna687@gmail.com>
2025-09-13 07:54:37 +0530

    added customer support

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -50,24 +50,24 @@ const Login = () => {
     }
   },[token])
   return (
-    <form onSubmit={onsubmithandler} className='flex mt-[100px] flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+    <form onSubmit={onsubmithandler} className='flex mt-[100px] flex-col items-center w-[90%] p-5 sm:max-w-96 m-auto mt-14 gap-4 border border-gray-400 rounded-xl text-gray-800'>
 
-      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+      <div className='inline-flex text-center items-center gap-2 mb-2 mt-8'>
         <p className='prata-regular text-[#c2410c] text-3xl'>{currentstate}</p>
         <p className='w-8 flex justify-center items-center sm:w-12 h-[1px] sm:h-[2px] bg-gray-700'>
           <img src={assests.flute} alt="" />
         </p>
       </div>
 
-      {currentstate === "Login" ? "" : <input type="text" className='w-full px-3 py-2 border border-gray-800'onChange={(e) => setName(e.target.value)} value={username} placeholder='Username' required />}
-      <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required />
+      {currentstate === "Login" ? "" : <input type="text" className='w-full px-3 py-2 border rounded-md border-gray-800'onChange={(e) => setName(e.target.value)} value={username} placeholder='Username' required />}
+      <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} className='w-full px-3 py-2 border rounded-md border-gray-800' placeholder='Email' required />
       <div className='relative w-full'>
-      <input type={showpassword ? "text": "password"} onChange={(e) => setPassword(e.target.value)} value={password} className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required />
+      <input type={showpassword ? "text": "password"} onChange={(e) => setPassword(e.target.value)} value={password} className='w-full px-3 py-2 rounded-md border border-gray-800' placeholder='Password' required />
       <button className='absolute right-5 top-2 text-gray-500 text-2xl cursor-pointer' onClick={() => setShowPassword(prev => !prev)}>{!showpassword ? <IoMdEye />: <IoIosEyeOff />}</button>
       </div>
       <div className='w-full flex justify-between text-sm mt-[-8px]'>
         {
-          currentstate === "Sign-up" ? <p className='cursor-pointer hover:text-[#c2410c]'>Forgot Password?</p> : null
+          currentstate === "Login" ? <p onClick={() => navigate('/forgot-password')} className='cursor-pointer hover:text-[#c2410c]'>Forgot Password?</p> : null
         }
   
         {
@@ -76,10 +76,10 @@ const Login = () => {
             : <p onClick={() => setcurrentstate("Login")} className='cursor-pointer hover:text-[#c2410c]'>Already have an account?</p>
         }
       </div>
-      <button className='bg-[#c2410c] cursor-pointer text-white border rounded-lg font-light px-8 py-2 mt-4'>{currentstate === "Login" ? "Sign-In" : "Register"}</button>
+      <button className='bg-[#c2410c] cursor-pointer text-white border rounded-lg font-light px-8 py-2 mt-1'>{currentstate === "Login" ? "Sign-In" : "Register"}</button>
 
        {/* Divider */}
-      <div className='flex items-center w-full my-4'>
+      <div className='flex items-center w-full my-1'>
         <hr className='flex-grow border-gray-400' />
         <span className='px-2 text-sm text-gray-500'>OR</span>
         <hr className='flex-grow border-gray-400' />
@@ -89,20 +89,12 @@ const Login = () => {
       <button
         onClick={() => window.open("http://localhost:4000/api/auth/google", "_self")}
         type="button"
-        className='flex items-center cursor-pointer gap-2 bg-white border border-gray-400 rounded-lg shadow px-4 py-2 w-full justify-center hover:bg-gray-100'
+        className='flex items-center cursor-pointer gap-2 bg-white mb-1 border border-gray-400 rounded-lg shadow px-2 py-2 w-full justify-center hover:bg-gray-100'
       >
         <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Logo" className='w-5 h-5' />
         <span className='text-gray-700'>Continue with Google</span>
       </button>
 
-      {/* Facebook Login */}
-      <button
-        type="button"
-        className='flex cursor-pointer items-center gap-2 bg-[#1877F2] rounded-lg shadow px-4 py-2 w-full justify-center hover:bg-blue-600'
-      >
-        <img src="https://www.svgrepo.com/show/448224/facebook.svg" alt="Facebook Logo" className='w-5 h-5 bg-white rounded-full p-[2px]' />
-        <span className='text-white'>Continue with Facebook</span>
-      </button>
     </form>
   )
 }
