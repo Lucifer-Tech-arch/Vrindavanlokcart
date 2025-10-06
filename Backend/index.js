629a7f25d6cd4fd899647c965fcbbccedfb66f32
Krishna Goswami <www.goswamikrishna687@gmail.com>
2025-10-06 10:39:07 +0530

    updated port

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ import reviewRouter from "./routes/reviewroute.js";
 // app config
 const app = express();
 
-//const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 connectdb();
 configcloudinary();
@@ -44,8 +44,8 @@ app.use((req,res)=> {
 })
 
 
-/*app.listen(port, () => {
+app.listen(port, () => {
   console.log("Server running on PORT: "+port);
-})*/
+})
 
-export default app;
+//export default app;
